24d63cf325dd297cf8f44923cadce00b1498303a
fuadhaider <mohaiderfuad@gmail.com>
2017-11-18 23:10:29 +0000

    section: main, aside, nav frame

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-
+import { SectionComponent } from './section/section.component';
+import { NavComponent } from './section/nav/nav.component';
+import { MainComponent } from './section/main/main.component';
+import { AsideComponent } from './section/aside/aside.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SectionComponent,
+    NavComponent,
+    MainComponent,
+    AsideComponent
   ],
   imports: [
     BrowserModule,
