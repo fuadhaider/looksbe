13ebdd74fa6f0e175bcea514c20e299e2a2eaa20
fuadhaider <mohaiderfuad@gmail.com>
2017-11-22 17:21:31 +0000

    mock-data display

--- src/app/section/main/main.component.ts
+++ src/app/section/main/main.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../../hero';
+import { HEROES } from '../../mock-heroes';
+
 
 @Component({
   selector: 'app-main',
@@ -6,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
+  heroes = HEROES;
 
   constructor() { }
 
