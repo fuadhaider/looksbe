5c0a2ea7322b0e63ae4ecd5db6b4ac56a4e72750
fuadhaider <mohaiderfuad@gmail.com>
2018-01-02 12:27:46 +0000

    commit

--- src/app/section/main/main.component.ts
+++ src/app/section/main/main.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ViewContainerRef } from '@angular/core';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
+  // directives: ['app-login-nav']
 })
 export class MainComponent implements OnInit {
+  @ViewChild('appLoginNav') appLoginNav: ViewContainerRef;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  destroyClick() {
+    if (this.appLoginNav) {
+      this.appLoginNav.remove();
+    }
   }
 
 }
